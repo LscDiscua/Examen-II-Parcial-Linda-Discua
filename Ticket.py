9d4b1ffc055edf82aa49707e1523d3fe6025d893
Linda Discua <lindadiscua1998@gmail.com>
2020-03-13 18:08:33 -0600

    Clase Ticket Modificada

--- Ticket.py
+++ Ticket.py
@@ -11,14 +11,24 @@ class Ticket:
 
     def __init__(self):
         """ Representa el inicio de los valore del programa en este caso del vehiculo."""
-        self.vehiculo = list()
+        self.vehiculos = list()
     
-    def agregar_vehiculo(self):
+    def agregar(self):
         """ Se Creara la funcion para ingresar los valores del vehiculo. """"
-        self.vehiculo.append(Vehiculo(numero_de_placa,marca,modelo,tipo_vehiculo,fecha_ingreso,hora_ingreso, estado))
+        self.vehiculos.append(Vehiculo(numero_de_placa,marca,modelo,tipo_vehiculo,fecha_ingreso,hora_ingreso, estado))
+    
+    def _buscar_vehiculo(self, numero_de_placa):
+        """ Buscar un vehiculo en especicfico se creara una clase privada para ello """
+         for vehiculo in self.vehiculos:
+             if numero.placa == numero_de_placa:
+                 return vehiculo
+        return None
+    
     
     def buscar_vehiculo(self, filter):
         """ Se buscara o se encontraran todos los valores del vehiculo.""""
+        return [vehiculo for vehiculo in self.vehiculos if note.buscar(filter)]
+
     
 
 
